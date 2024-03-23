c8b0e8107a28f1d7d1bac9b8a00faab7b99d86dd
anigno <anigno@gmail.com>
2024-03-24 00:41:30 +0200

    esp now sender and receiver simple message sending works

--- esp32_target_practice/espnow_received/espnow_received.ino
+++ esp32_target_practice/espnow_received/espnow_received.ino
@@ -1,51 +1,34 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define LED_PIN 2
-int cnt = 0;
+#define LED_PIN 2  // Pin connected to LED
 
-void blink_n_times(int n) {
-  pinMode(2, OUTPUT);
-  for (int i = 0; i < n; i++) {
-    digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(100);                   // wait for a second
-    digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(200);
-  }
-}
-
-//**receive espnow message. "blink_n", blink n times*/
 void onDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  Serial.print("Received message");
-  String receivedString = "";
-  for (int i = 0; i < data_len; i++) {
-    receivedString += (char)data[i];
-  }
-  if (receivedString.startsWith("blink_")) {
-    int numBlinks = receivedString.substring(6).toInt();
-    Serial.print("Received blink command. Number of blinks: ");
-    Serial.println(numBlinks);
-    blink_n_times(numBlinks);
-  }
+  Serial.println("Message received");
+  digitalWrite(LED_PIN, HIGH);  // Turn on LED
+  delay(250);                  // Keep LED on for 1 second
+  digitalWrite(LED_PIN, LOW);   // Turn off LED
 }
 
-/////////////////////////////////////////////////////////
 void setup() {
-  //Initialize Serial Monitor
   Serial.begin(115200);
-  //Set device as a Wi-Fi Station
+  pinMode(LED_PIN, OUTPUT);
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
   WiFi.mode(WIFI_STA);
-  //Init ESP-NOW
+
+  // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+
+  // Register callback function to handle received data
   esp_now_register_recv_cb(onDataRecv);
-  blink_n_times(3);
 }
-/////////////////////////////////////////////////////////
+int cnt = 0;
 void loop() {
-  // cnt = (cnt + 1) % 20;
-  // digitalWrite(LED_PIN, cnt / 10);
-  // delay(100);
+  // Do other stuff if needed
+  Serial.println(cnt++);
+  delay(1000);
 }
--- esp32_target_practice/espnow_sender/espnow_sender.ino
+++ esp32_target_practice/espnow_sender/espnow_sender.ino
@@ -1,42 +1,78 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define LED_PIN 2
-uint8_t receiverMacAddress1[] = { 0xE4, 0x65, 0xB8, 0x83, 0xEC, 0xF4 };  // Replace with your receiver's MAC address
-int cnt = 0;
-//E4:65:B8:83:EC:F4
-void registerPeer(uint8_t *macAddress) {
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, macAddress, 6);
-  peerInfo.channel = 0;  // Use channel 0
-  peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-  }
-}
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0xE4, 0x65, 0xB8, 0x83, 0xEC, 0xF4};
+uint8_t broadcastAddress2[] = {0x08, 0xD1, 0xF9, 0x3B, 0x38, 0x25};
+uint8_t broadcastAddress3[] = {0x08, 0xD1, 0xF9, 0x3B, 0x38, 0x26};
 
-void sendMessage(uint8_t *macAddress, String message) {
-  esp_now_send(macAddress, (uint8_t *)message.c_str(), message.length());
-  Serial.print("sent ");
-  Serial.println(message);
-}
+typedef struct test_struct {
+  int x;
+  int y;
+} test_struct;
 
+test_struct test;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
 void setup() {
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
-
+ 
   WiFi.mode(WIFI_STA);
+ 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  registerPeer(receiverMacAddress1);
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // register second peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  /// register third peer
+  memcpy(peerInfo.peer_addr, broadcastAddress3, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
-
+ 
 void loop() {
-  cnt = (cnt + 1) % 2;
-  digitalWrite(LED_PIN, cnt);
-  Serial.println(cnt);
-  sendMessage(receiverMacAddress1, "blink_3");  // Change the blink count as needed
-  delay(2000);
+  test.x = random(0,20);
+  test.y = random(0,20);
+ 
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(4000);
 }
